33eea9d614a45403dd37bead9a0708b75e440259
Angel Jimenez <angeljpp29@gmail.com>
2019-10-02 02:01:25 -0600

    Update zigzag encryption

---
+++ Laboratorio2/Laboratorio2/Compresion/ZigZag.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Laboratorio2.DBContext;
+
+namespace Laboratorio2.Compresion
+{
+    public class ZigZag
+    {
+        private int iClave;
+
+        public ZigZag()
+        {
+            iClave = default(int);
+        }
+
+        public ZigZag(int clave)
+        {
+            iClave = clave;
+        }
+
+        public void Comprimir(FileInfo file)
+        {
+            int M = 0;
+            M = CalcularM(file.Length);
+        }
+
+        private int CalcularM(long lengthArchivo)
+        {
+            double M = 0;
+
+            M=((double)(Convert.ToDouble(lengthArchivo)+1+(2*((double)iClave-2))))/((double)(2+ (2 * ((double)iClave - 2)))
+
+            return (int)Math.Ceiling(M);
+        }
+    }
+}
---
+++ Laboratorio2/Laboratorio2/DBContext/DefaultConnection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio2.DBContext
+{
+    public class DefaultConnection
+    {
+        private static volatile DefaultConnection Instance;
+        private static object syncRoot = new Object();
+
+        private static FileInfo fileInfo = default(FileInfo);
+
+        //public List<Models.Archivo> historialCompresiones = new List<Models.Archivo>();
+        public int IdActual { get; set; }
+
+        public DefaultConnection()
+        {
+            IdActual = 0;
+        }
+
+        public void AsignarRuta(FileInfo file)
+        {
+            fileInfo = file;
+        }
+
+        public FileInfo ObtenerRuta()
+        {
+            return fileInfo;
+        }
+
+        public static DefaultConnection getInstance
+        {
+            get
+            {
+                if (Instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (Instance == null)
+                        {
+                            Instance = new DefaultConnection();
+                        }
+                    }
+                }
+                return Instance;
+            }
+        }
+    }
+}
