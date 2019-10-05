a29d348b6c62c66fd95fbb290e0e301e7400d4e9
Pablohe18 <31459420+Pablohe18@users.noreply.github.com>
2019-10-04 20:37:49 -0600

    Agregando metodo CESAR

---
+++ Laboratorio2/Laboratorio2/Cifrado/Cesar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio2.Cifrado
+{
+    public class Cesar
+    {
+        public static String CifrarCadena(String text)
+        {
+            int valorCaracter, letras;
+            string cadena = text, cifrado = "";
+            letras = cadena.Length;
+            char[] ch = new char[letras];
+            for (int i = 0; i < letras; i++)
+            {
+                valorCaracter = (int)cadena[i];
+                ch[i] = (char)(valorCaracter + 3 * letras);
+                cifrado += ch[i];
+            }
+            return cifrado;
+        }
+
+        public static String DecifrarCadena(String textCifrado)
+        {
+            int valorCaracter, letras;
+            string cadena = textCifrado, descifrado = "";
+            letras = cadena.Length;
+            char[] ch = new char[letras];
+            for (int i = 0; i < letras; i++)
+            {
+                valorCaracter = (int)cadena[i];
+                ch[i] = (char)(valorCaracter - 3 * letras);
+                descifrado += ch[i];
+            }
+            return descifrado;
+        }
+    }
+}
---
+++ Laboratorio2/Laboratorio2/Controllers/CesarController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Laboratorio2.DBContext;
+
+namespace Laboratorio2.Controllers
+{
+    public class CesarController : Controller
+    {
+        DefaultConnection db = DefaultConnection.getInstance;
+        // GET: Cesar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+
+        //SUBIR DE ARCHIVO 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UploadFile(HttpPostedFileBase File)
+        {
+            string filePath = string.Empty;
+            if (File != null)
+            {
+                string path = Server.MapPath("~/UploadedFiles/");
+                filePath = path + Path.GetFileName(File.FileName);
+                string extension = Path.GetExtension(File.FileName);
+                File.SaveAs(filePath);
+                ViewBag.Message = "Archivo Cargado";
+
+                FileInfo fileInfo = new FileInfo(filePath);
+
+                string nombre_original = fileInfo.Name;
+                long tamanio_original = fileInfo.Length;
+
+                db.AsignarRuta(fileInfo);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
