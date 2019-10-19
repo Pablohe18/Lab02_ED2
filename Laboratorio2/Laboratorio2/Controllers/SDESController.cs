add794627208a219d06434f114d1316d15cf135f
Angel Jimenez <angeljpp29@gmail.com>
2019-10-18 23:40:33 -0600

    Update GetKey

--- Laboratorio2/Laboratorio2/Controllers/SDESController.cs
+++ Laboratorio2/Laboratorio2/Controllers/SDESController.cs
@@ -4,17 +4,46 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Laboratorio2.DBContext;
+using Laboratorio2.Models;
+using Laboratorio2.Cifrado;
+using System.Net;
+using System.IO;
 
 namespace Laboratorio2.Controllers
 {
     public class SDESController : Controller
     {
         DefaultConnection db = DefaultConnection.getInstance;
+        SDes cifradoSDes = new SDes();
         // GET: SDES
         public ActionResult Index()
         {
             return View(db.ObtenerLista());
         }
+
+        //GET: SDES/GetKey
+        public ActionResult GetKey()
+        {
+            return View();
+        }
+        //POST: SDES/GetKey
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GetKey([Bind(Include ="Key")]Keysdes keysdes)
+        {
+            if (ModelState.IsValid)
+            {
+                if (keysdes.Key > 0 && keysdes.Key < 1024) 
+                {
+                    db.keysdes = keysdes.Key;
+                    //return RedirectToAction("UploadFile");
+                }
+                ViewBag.Message = "Debe ingresar un numero mayor a 0 y menor a 1024";
+            }
+            return View(keysdes);
+        }
+
+
 
         // GET: SDES/Details/5
         public ActionResult Details(int id)
