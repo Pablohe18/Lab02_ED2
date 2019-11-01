f89752da5e37cbd38c146a4221edc0091b1202da
Angel Jimenez <angeljpp29@gmail.com>
2019-11-01 13:16:41 -0600

    Create KeyRSA.cs

---
+++ Laboratorio2/Laboratorio2/Models/KeyRSA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio2.Models
+{
+    public class KeyRSA
+    {
+        public int keyP { get; set; }
+        public int KeyQ { get; set; }
+    }
+}
