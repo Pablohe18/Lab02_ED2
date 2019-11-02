ee926739775c69b464b5f6c5f11ecb9e063d6ce9
Angel Jimenez <angeljpp29@gmail.com>
2019-11-02 04:38:33 -0600

    Update Encrypt File

--- Laboratorio2/Laboratorio2/Controllers/AlgoritmoRSAController.cs
+++ Laboratorio2/Laboratorio2/Controllers/AlgoritmoRSAController.cs
@@ -8,6 +8,7 @@ using Laboratorio2.DBContext;
 using Laboratorio2.Cifrado;
 using Laboratorio2.Models;
 using System.IO;
+using System.Text;
 
 namespace Laboratorio2.Controllers
 {
@@ -28,6 +29,9 @@ namespace Laboratorio2.Controllers
             return View(db.ObtenerLista());
         }
 
+        ////////
+        //METODOS PARA LA GENERACION Y OBTENCION DE LLAVES
+        ////////
 
         //GET: SDES/GetKey
         [HttpGet]
@@ -98,5 +102,167 @@ namespace Laboratorio2.Controllers
             return File(ruta, "text/plain", "private.key");
         }
 
+        ////////
+        //METODOS PARA CIFRAR RSA
+        ////////
+
+        //GET: SDES/EncryptFile
+        [HttpGet]
+        public ActionResult ReadPublicKey()
+        {
+            return View();
+        }
+
+        //SUBIR DE ARCHIVO PARA CIFRAR
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ReadPublicKey(HttpPostedFileBase File)
+        {
+            string filePath = string.Empty;
+            if (File != null)
+            {
+                string path = Server.MapPath("~/UploadedFiles/");
+                filePath = path + Path.GetFileName(File.FileName);
+                string extension = Path.GetExtension(File.FileName);
+                File.SaveAs(filePath);
+                ViewBag.Message = "Archivo Cargado";
+
+                FileInfo fileInfo = new FileInfo(filePath);
+
+                string builder = default(string);
+
+                StreamReader reader = new StreamReader(fileInfo.FullName);
+                while ((builder = reader.ReadLine()) != null)
+                {
+                    cifradoRSA.keys[0] = Convert.ToInt64(builder.Split(',')[0]);
+                    cifradoRSA.keys[1] = Convert.ToInt64(builder.Split(',')[1]);
+                }
+                reader.Close();
+
+                return RedirectToAction("EncryptFile");
+            }
+
+            return View();
+        }
+
+
+        //GET: SDES/EncryptFile
+        [HttpGet]
+        public ActionResult EncryptFile()
+        {
+            return View();
+        }
+
+        //SUBIR DE ARCHIVO PARA CIFRAR
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EncryptFile(HttpPostedFileBase File)
+        {
+            string filePath = string.Empty;
+            if (File != null)
+            {
+                string path = Server.MapPath("~/UploadedFiles/");
+                filePath = path + Path.GetFileName(File.FileName);
+                string extension = Path.GetExtension(File.FileName);
+                File.SaveAs(filePath);
+                ViewBag.Message = "Archivo Cargado";
+
+                FileInfo fileInfo = new FileInfo(filePath);
+                db.AsignarRuta(fileInfo);
+                StringBuilder builder = new StringBuilder();
+
+                var buffer = new char[bufferLength];
+                using (var file = new FileStream(db.ObtenerRuta().FullName, FileMode.Open))
+                {
+                    using (var reader = new BinaryReader(file))
+                    {
+                        while (reader.BaseStream.Position != reader.BaseStream.Length)
+                        {
+                            buffer = reader.ReadChars(bufferLength);
+                            foreach (var item in buffer)
+                            {
+                                builder.Append(((char)cifradoRSA.cipher((int)item)).ToString());
+                            }
+                        }
+
+                    }
+
+                }
+
+                ////////
+                var ruta = Server.MapPath("~/DownloadedFiles/") + db.ObtenerRuta().Name.Split('.')[0] + ".rsacif";
+                using (StreamWriter outputFile = new StreamWriter(ruta))
+                {
+                    foreach (char caracter in builder.ToString())
+                    {
+                        outputFile.Write(caracter.ToString());
+                    }
+                }
+
+                db.AsignarRuta(new FileInfo(ruta));
+
+                return RedirectToAction("DownloadFile");
+            }
+
+            return View();
+        }
+
+        ////////
+        //METODOS PARA CIFRAR RSA
+        ////////
+
+        //GET: SDES/EncryptFile
+        [HttpGet]
+        public ActionResult ReadPrivateKey()
+        {
+            return View();
+        }
+
+        //SUBIR DE ARCHIVO PARA CIFRAR
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ReadPrivateKey(HttpPostedFileBase File)
+        {
+            string filePath = string.Empty;
+            if (File != null)
+            {
+                string path = Server.MapPath("~/UploadedFiles/");
+                filePath = path + Path.GetFileName(File.FileName);
+                string extension = Path.GetExtension(File.FileName);
+                File.SaveAs(filePath);
+                ViewBag.Message = "Archivo Cargado";
+
+                FileInfo fileInfo = new FileInfo(filePath);
+
+                string builder = default(string);
+
+                StreamReader reader = new StreamReader(fileInfo.FullName);
+                while ((builder = reader.ReadLine()) != null)
+                {
+                    cifradoRSA.keys[0] = Convert.ToInt64(builder.Split(',')[0]);
+                    cifradoRSA.keys[1] = Convert.ToInt64(builder.Split(',')[1]);
+                }
+                reader.Close();
+
+                return RedirectToAction("EncryptFile");
+            }
+
+            return View();
+        }
+
+
+        //METODO PARA DESCARGAR EL ARCHIVO
+        public ActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        //DESCARGAR EL ARCHIVO
+        public FileResult Download()
+        {
+            var ruta = Server.MapPath("~/DownloadedFiles/") + db.ObtenerRuta().Name;
+            return File(ruta, "text/plain", db.ObtenerRuta().Name);
+        }
     }
 }
+
